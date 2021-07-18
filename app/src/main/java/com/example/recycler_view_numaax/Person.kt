7e5ec81a8fd63bfe562609df5dbabe19bd2f4e76
NuMaax <77022580+NuMaax@users.noreply.github.com>
2021-07-18 07:39:57 +0300

    Revert "Workspace person"

--- app/src/main/java/com/example/recycler_view_numaax/Person.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recycler_view_numaax
-
-data class Person(
-    val imageId: Int,
-    val personName: String,
-    val personAge: String
-    )
--- app/src/main/java/com/example/recycler_view_numaax/PlantAdapter.kt
+++ app/src/main/java/com/example/recycler_view_numaax/PlantAdapter.kt
@@ -4,52 +4,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recycler_view_numaax.databinding.NamesItemBinding
+import com.example.recycler_view_numaax.databinding.PlantItemBinding
 
-class PersonAdapter : RecyclerView.Adapter<PersonAdapter.PlantHolder>() {
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
 
     // список, откуда адаптер берет данные, может быть БД
-    private val personList = ArrayList<Person>()
+    private val plantList = ArrayList<Plant>()
 
     // наполнение. изменить в "fun bind"
     class PlantHolder(item: View) : RecyclerView.ViewHolder(item) {
 
-        private val binding = NamesItemBinding.bind(item)
+        private val binding = PlantItemBinding.bind(item)
 
-        fun bind(person: Person) {
-            binding.imPersonPhoto.setImageResource(person.imageId)
-            binding.tvPersonName.text = person.personName
-            binding.tvPersonAge.text = person.personAge
+        fun bind(plant: Plant) {
+            binding.im.setImageResource(plant.imageId)
+            binding.tvTitle.text = plant.title
         }
     }
-
     // изменить plant_item. эта ф-ция берет пустой шаблон и надувает его
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.names_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.plant_item, parent, false)
         return PlantHolder(view)
     }
-
-    // изменить personList.
+    // изменить plantList.
     override fun onBindViewHolder(holder: PlantHolder, position: Int) {
-        holder.bind(personList[position])
+        holder.bind(plantList[position])
     }
 
-    // изменить personList
+    // изменить plantList
     override fun getItemCount(): Int {
-        return personList.size
+        return plantList.size
     }
 
-    // доп. ф-ция, добавляет элемент в массив, изменить (person: Person), personList
-    fun addPerson(person: Person) {
-        personList.add(person)
+    // доп. ф-ция, добавляет элемент в массив, изменить (plant: Plant), plantList
+    fun addPlant(plant: Plant){
+        plantList.add(plant)
         notifyDataSetChanged()
     }
 
     // https://youtu.be/4pevVON0v-U?list=PLmjT2NFTgg1fdHN-9Wn4XYr-IOuadxMm5&t=3291
     // добавление целого списка сразу
-    fun addAll(list: List<Person>) {
-        personList.clear()
-        personList.addAll(list)
+    fun addAll(list: List<Plant>){
+        plantList.clear()
+        plantList.addAll(list)
         notifyDataSetChanged()
 
     }
